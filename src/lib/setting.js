2425629e15c327022fbc47de75e432c42a0bebfd
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-03-24 23:12:17 +0530

    update

--- src/lib/setting.js
+++ src/lib/setting.js
@@ -1,4 +1,4 @@
-export const baseURL = process.env.NEXT_PUBLIC_BASE_API_URL
+export const baseURL = 'http://localhost:3000/api/'
 export const authBaseURL = process.env.NEXT_PUBLIC_AUTH_API_URL
 export const apiKey = process.env.NEXT_PUBLIC_API_KEY
 export const publicApiUrl = process.env.NEXT_PUBLIC_DB_URL
