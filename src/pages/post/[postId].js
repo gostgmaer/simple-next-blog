0e53f6508a7817137431c13e7783ce0a7231f50d
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-03-25 10:21:48 +0530

    updated Images

--- src/pages/post/[postId].js
+++ src/pages/post/[postId].js
@@ -1,5 +1,5 @@
 import Auther from "@/components/childs/Auther";
-import PostItem from "@/components/childs/Postlist";
+import PostList from "@/components/childs/Postlist";
 import Error from "@/components/error/Error";
 import Loading from "@/components/loaderscreen/Loading";
 import Layout from "@/layout";
@@ -26,62 +26,27 @@ const Post = () => {
         </div>
         <div className="post py-10">
           <h1 className=" font-bold text-4xl text-center">
-            You have been logged out due to inactivity.
+           {data.title}
           </h1>
           <p className=" text-grey-500 text-center text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-            quidem temporibus doloribus in at ipsum animi deserunt sequi
+           {data.subtitle}
           </p>
           <div className="py-10">
             <Image
-              src={"/assets/images/img1.jpg"}
+              src= {data.img}
               alt=""
               className="rounded"
               width={"900"}
               height={"600"}
             />
           </div>
-          <div className="content text-gray-600 text-lg flex flex-col gap-5">
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
-              saepe atque corrupti libero molestias! Praesentium iure,
-              consectetur ratione ex non repellendus voluptate in quas, eius quo
-              dolores nisi ducimus ullam!
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
-              saepe atque corrupti libero molestias! Praesentium iure,
-              consectetur ratione ex non repellendus voluptate in quas, eius quo
-              dolores nisi ducimus ullam!
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
-              saepe atque corrupti libero molestias! Praesentium iure,
-              consectetur ratione ex non repellendus voluptate in quas, eius quo
-              dolores nisi ducimus ullam!
-            </p>
-
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
-              saepe atque corrupti libero molestias! Praesentium iure,
-              consectetur ratione ex non repellendus voluptate in quas, eius quo
-              dolores nisi ducimus ullam!
-            </p>
-          </div>
+          <div
+            dangerouslySetInnerHTML={{ __html: data.description }}
+            className="content whitespace-pre-line text-gray-600 text-lg flex flex-col gap-5"
+          ></div>
         </div>
         <div className="comment"></div>
-        <div className="related py-10">
-          <h2 className=" font-bold text-2xl py-12 text-start">
-            Related Posts
-          </h2>
-          <div className="flex flex-col gap-6">
-            {/* <PostItem />
-            <PostItem />
-            <PostItem />
-            <PostItem />
-            <PostItem /> */}
-          </div>
-        </div>
+        <PostList endpoint={"post"} title={"Related Posts"} cate />
       </section>
     </Layout>
   );
