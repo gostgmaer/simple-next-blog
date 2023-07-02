6cef193087e0b961f98cfe345fd100092235699a
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-07-02 21:19:24 +0530

    updated json

--- src/components/sections/sectiontwo.js
+++ src/components/sections/sectiontwo.js
@@ -6,22 +6,14 @@ import Auther from "../childs/Auther";
 import Error from "../error/Error";
 import Loading from "../loaderscreen/Loading";
 
-const Sectiontwo = ({ data, isLoading, isError }) => {
-  // getPost('post',69).then(res=>console.log(res))
-  // const { data, isError, isLoading } = useFetcher("post");
-
-  // console.log(getPost('post'));
-  if (isLoading) {
-    return <Loading />;
-  }
-  if (isError) {
-    return <Error />;
-  }
+const Sectiontwo = ({ data }) => {
+ 
+  console.log(data);
   return (
     <section className="py-16 container mx-auto md:px-20">
       <h2 className=" font-bold text-4xl py-12 text-center">Latest Posts</h2>
       <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {data?.map((item) => (
+        {data?.data?.map((item) => (
           <Post key={item.id} data={item} />
         ))}
       </div>
@@ -32,17 +24,19 @@ const Sectiontwo = ({ data, isLoading, isError }) => {
 export default Sectiontwo;
 
 const Post = ({ data }) => {
+  console.log(data.featured_image_urls.medium[0]);
   return (
     <div className="item">
       <div className="images">
         <Link href={`/post/${data.id}`}>
-          <Image
-            src={data.img}
+        <img src={`${data.featured_image_urls.medium[0]}`} alt="asdasd" />
+        {/* <Image
+            src={`${data.featured_image_urls.medium[0]}`}
             alt=""
             className="rounded"
             width={"600"}
             height={"600"}
-          />
+          /> */}
         </Link>
       </div>
       <div className="info justify-center flex flex-col py-4">
@@ -51,23 +45,24 @@ const Post = ({ data }) => {
             <Link
               className=" text-orange-600 hover:text-orange-800 "
               href={"/cate"}
+            
             >
-              {data.category}
+              {}
             </Link>
           }
           <Link className=" text-grey-600 hover:text-grey-800" href={"/cate"}>
-            - {data.published}
+            {/* - {data.published} */}
           </Link>
         </div>
         <div className="title">
           <Link
-            href={`/post/${data.id}`}
+            href={`/post/${data.slug}`}
             className=" text-xl font-bold text-gray-800 hover:text-gray-600"
           >
-            {data.title}
+            {data.title.rendered}
           </Link>
-          <p className=" text-gray-500 py-0">{data.subtitle}</p>
-          <Auther user={data.author} />
+          {/* <p className=" text-gray-500 py-0">{data.subtitle}</p> */}
+          {/* <Auther user={data.author_info.name} /> */}
         </div>
       </div>
     </div>
--- src/lib/setting.js
+++ src/lib/setting.js
@@ -3,3 +3,6 @@ export const authBaseURL = process.env.NEXT_PUBLIC_AUTH_API_URL
 export const apiKey = process.env.NEXT_PUBLIC_API_KEY
 export const publicApiUrl = process.env.NEXT_PUBLIC_DB_URL
 export const placeHolder ='https://jsonplaceholder.typicode.com/posts'
+export const apiUrl ='http://localhost/blog/wp-json'
+export const apiUser ='admin'
+export const apipass ='Password@123'
